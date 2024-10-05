296e6d24c7a10322fdd1a0f8f3c98b3a6885e40a
Pakamon234 <tgddtn05111@gmail.com>
2024-10-06 06:43:27 +0700

    nothing

--- website/__init__.py
+++ website/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "hjshjhdjah kjshkjdhjs"
     # Cấu hình SQLAlchemy
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:thuong@localhost/qlnh"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:long%40091103@localhost/qlnh"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
