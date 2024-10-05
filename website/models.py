b0f206896bd44a8201f0b985e0f99a60dc35cd0b
ThuongPhan662003 <thuong5so55555@gmail.com>
2024-10-06 00:57:26 +0700

    chia task

--- website/models.py
+++ website/models.py
@@ -5,18 +5,18 @@ from sqlalchemy.sql import func
 
 class NhomNguoiDung(db.Model):
     __tablename__ = "NhomNguoiDung"
-    idNND = db.Column(db.Integer, primary_key=True)
+    MaNND = db.Column(db.Integer, primary_key=True)
     TenNhomNguoiDung = db.Column(db.String(100))
     NguoiDung_List = db.relationship("NguoiDung", backref="NhomNguoiDung", lazy=True)
 
 
 class NguoiDung(db.Model, UserMixin):
     __tablename__ = "NguoiDung"
-    idND = db.Column(db.Integer, primary_key=True)
+    MaND = db.Column(db.Integer, primary_key=True)
     UserName = db.Column(db.String(150))
     TrangThai = db.Column(db.Integer)
     MatKhau = db.Column(db.String(150))
     VerifyCode = db.Column(db.String(150))
-    idNND = db.Column(db.Integer, db.ForeignKey("NhomNguoiDung.idNND"), nullable=False)
+    idNND = db.Column(db.Integer, db.ForeignKey("NhomNguoiDung.MaNND"), nullable=False)
     def get_id(self):
-        return self.idND
+        return self.MaND
