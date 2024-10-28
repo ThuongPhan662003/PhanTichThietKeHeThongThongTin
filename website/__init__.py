32ca99341fb3bfebf1b6530ea59dac6b2f4866c8
ThuongPhan662003 <thuong5so55555@gmail.com>
2024-10-28 10:44:06 +0700

    add api, html

--- website/__init__.py
+++ website/__init__.py
@@ -3,17 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 import os
 from flask_login import LoginManager
+from authlib.integrations.flask_client import OAuth
+from .auth import init_oauth
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 DB_PATH = os.path.join("db", DB_NAME)
 
-# if not os.path.exists("db"):
-#     os.makedirs("db")
-
 
 def create_app():
-    app = Flask(__name__)
+    # from .models import NguoiDung, NhomNguoiDung
+
+    app = Flask(__name__, static_folder="assets")
+
     app.config["SECRET_KEY"] = "hjshjhdjah kjshkjdhjs"
     # Cấu hình SQLAlchemy
     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:thuong@localhost/qlnh"
@@ -23,10 +25,8 @@ def create_app():
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix="/view")
+    app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/auth")
-
-    from .models import NguoiDung, NhomNguoiDung
 
     with app.app_context():
         db.create_all()
@@ -34,11 +34,10 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return NguoiDung.query.get(int(id))
-
+    init_oauth(app)
+    # @login_manager.user_loader
+    # def load_user(id):
+    #     return NguoiDung.query.get(int(id))
     return app
 
 
--- website/views.py
+++ website/views.py
@@ -1,142 +1,147 @@
+# from .models import *
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import *
 from . import db
 import json
 
 views = Blueprint("views", __name__)
 
 
-@views.route("/functions")
-def get_functions():
-    functions = ChucNang.query.all()  # Truy vấn tất cả các chúc năng
-    results = [
-        {"MaCN": function.MaCN, "TenManHinh": function.TenManHinh}
-        for function in functions
-    ]
-    return jsonify(results)
+# @views.route("/functions")
+# def get_functions():
+#     functions = ChucNang.query.all()  # Truy vấn tất cả các chúc năng
+#     results = [
+#         {"MaCN": function.MaCN, "TenManHinh": function.TenManHinh}
+#         for function in functions
+#     ]
+#     return jsonify(results)
 
 
-@views.route("/users")
-def get_users():
-    users = NguoiDung.query.all()
-    results = [{"id": user.MaND, "name": user.UserName} for user in users]
-    return jsonify(results)
+# @views.route("/users")
+# def get_users():
+#     users = NguoiDung.query.all()
+#     results = [{"id": user.MaND, "name": user.UserName} for user in users]
+#     return jsonify(results)
 
 
-@views.route("/tables")
-def get_tables():
-    tables = Ban.query.all()
-    results = [
-        {
-            "idBan": table.MaBan,
-            "name": table.TenBan,
-            "ViTri": table.ViTri,
-            "TrangThai": table.TrangThai,
-        }
-        for table in tables
-    ]
-    return jsonify(results)
+# @views.route("/tables")
+# def get_tables():
+#     tables = Ban.query.all()
+#     results = [
+#         {
+#             "idBan": table.MaBan,
+#             "name": table.TenBan,
+#             "ViTri": table.ViTri,
+#             "TrangThai": table.TrangThai,
+#         }
+#         for table in tables
+#     ]
+#     return jsonify(results)
 
 
-@views.route("/orders")
-def get_orders():
-    orders = DonDatHang.query.all()
-    results = []
-    for order in orders:
-        results.append(
-            {
-                "idDDH": order.MaDDH,
-                "NgayDat": order.NgayDat,
-                "TrangThai": order.TrangThai,
-                "ThanhTien": str(order.ThanhTien),  # Chuyển đổi thành chuỗi nếu cần
-            }
-        )
-    return jsonify(results)
+# @views.route("/orders")
+# def get_orders():
+#     orders = DonDatHang.query.all()
+#     results = []
+#     for order in orders:
+#         results.append(
+#             {
+#                 "idDDH": order.MaDDH,
+#                 "NgayDat": order.NgayDat,
+#                 "TrangThai": order.TrangThai,
+#                 "ThanhTien": str(order.ThanhTien),  # Chuyển đổi thành chuỗi nếu cần
+#             }
+#         )
+#     return jsonify(results)
 
 
-@views.route("/staffs")
-def get_staffs():
-    staffs = NhanVien.query.all()
-    results = [
-        {
-            "idNV": staff.MaNV,
-            "HoTen": staff.HoNV + " " + staff.TenNV,
-            "SDT": staff.SDT,
-            "Email": staff.Email,
-            "TinhTrang": staff.TinhTrang,
-        }
-        for staff in staffs
-    ]
-    return jsonify(results)
+# @views.route("/staffs")
+# def get_staffs():
+#     staffs = NhanVien.query.all()
+#     results = [
+#         {
+#             "idNV": staff.MaNV,
+#             "HoTen": staff.HoNV + " " + staff.TenNV,
+#             "SDT": staff.SDT,
+#             "Email": staff.Email,
+#             "TinhTrang": staff.TinhTrang,
+#         }
+#         for staff in staffs
+#     ]
+#     return jsonify(results)
 
 
-@views.route("/dishes")
-def get_dishes():
-    dishes = MonAn.query.all()
-    results = [
-        {
-            "idMA": dish.MaMA,
-            "TenMonAn": dish.TenMonAn,
-            "DonGia": str(dish.DonGia),
-            "Loai": dish.Loai,
-            "TrangThai": dish.TrangThai,
-        }
-        for dish in dishes
-    ]
-    return jsonify(results)
+# @views.route("/dishes")
+# def get_dishes():
+#     dishes = MonAn.query.all()
+#     results = [
+#         {
+#             "idMA": dish.MaMA,
+#             "TenMonAn": dish.TenMonAn,
+#             "DonGia": str(dish.DonGia),
+#             "Loai": dish.Loai,
+#             "TrangThai": dish.TrangThai,
+#         }
+#         for dish in dishes
+#     ]
+#     return jsonify(results)
 
 
-@views.route("/orderdetails")
-def get_orderdetails():
-    orderdetails = CT_DonDatHang.query.all()
-    results = [
-        {
-            "idDDH": orderdetail.idDDH,
-            "idBan": orderdetail.idBan,
-            "SoLuong": orderdetail.SoLuong,  # Nếu bạn có trường số lượng
-            "Gia": str(orderdetail.Gia),  # Nếu bạn có trường giá
-        }
-        for orderdetail in orderdetails
-    ]
-    return jsonify(results)
+# @views.route("/orderdetails")
+# def get_orderdetails():
+#     orderdetails = CT_DonDatHang.query.all()
+#     results = [
+#         {
+#             "idDDH": orderdetail.idDDH,
+#             "idBan": orderdetail.idBan,
+#             "SoLuong": orderdetail.SoLuong,  # Nếu bạn có trường số lượng
+#             "Gia": str(orderdetail.Gia),  # Nếu bạn có trường giá
+#         }
+#         for orderdetail in orderdetails
+#     ]
+#     return jsonify(results)
 
 
-@views.route("/customers")
-def get_customers():
-    customers = KhachHang.query.all()
-    results = [
-        {
-            "idKH": customer.MaKH,
-            "HoTen": customer.HoKH + " " + customer.TenKH,
-            "SDT": customer.SDT,
-            "Email": customer.Email,
-            "DiemTieuDung": customer.DiemTieuDung,
-            "DiemTichLuy": customer.DiemTichLuy,
-        }
-        for customer in customers
-    ]
-    return jsonify(results)
+# @views.route("/customers")
+# def get_customers():
+#     customers = KhachHang.query.all()
+#     results = [
+#         {
+#             "idKH": customer.MaKH,
+#             "HoTen": customer.HoKH + " " + customer.TenKH,
+#             "SDT": customer.SDT,
+#             "Email": customer.Email,
+#             "DiemTieuDung": customer.DiemTieuDung,
+#             "DiemTichLuy": customer.DiemTichLuy,
+#         }
+#         for customer in customers
+#     ]
+#     return jsonify(results)
 
 
-@views.route("/bills")
-def get_bills():
-    bills = HoaDon.query.all()
-    results = [
-        {
-            "idHD": bill.MaHD,
-            "NgayXuat": bill.NgayXuat,
-            "TongTien": str(bill.TongTien),
-            "TrangThai": bill.TrangThai,
-            "DiemCong": bill.DiemCong,
-            "DiemTru": bill.DiemTru,
-        }
-        for bill in bills
-    ]
-    return jsonify(results)
+# @views.route("/bills")
+# def get_bills():
+#     bills = HoaDon.query.all()
+#     results = [
+#         {
+#             "idHD": bill.MaHD,
+#             "NgayXuat": bill.NgayXuat,
+#             "TongTien": str(bill.TongTien),
+#             "TrangThai": bill.TrangThai,
+#             "DiemCong": bill.DiemCong,
+#             "DiemTru": bill.DiemTru,
+#         }
+#         for bill in bills
+#     ]
+#     return jsonify(results)
 
 
-@views.route("/", methods=["GET", "POST"])
-@login_required
-def home():
-    return render_template("home.html")
+# @views.route("/", methods=["GET", "POST"])
+# # @login_required
+# def home():
+#     return render_template("home.html")
+
+
+# @views.route("/homepage", methods=["GET", "POST"])
+# def homepage():
+#     return render_template("home_test.html")
